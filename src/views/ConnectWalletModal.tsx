7d69a4b9749110a837f9cf45ba74898e5925b532
Yuushin <Shyy>
2024-01-02 21:13:02 +0800

    update the wallet connect modal and display it automatically if no connecting...

--- src/views/ConnectWalletModal.tsx
+++ src/views/ConnectWalletModal.tsx
@@ -16,26 +16,45 @@ export const ConnectWalletModal = ({
 		connect({ connector: connectors[parseInt(evt.currentTarget.id)] });
 	};
 
-	return isOpen ? <Modal
-		title={t("connectWallet")}
-		onClose={onClose}>
-		{connectors.map((connector, index) => {
-			let name = connector.name;
-
-			// 判断是不是ioPay
-			if (name === "Trust Wallet" && window.navigator.userAgent.indexOf("IoPay") >= 0) {
-				name = "ioPay";
-			}
-
-			return <button
-				key={connector.name}
-				id={String(index)}
-				disabled={!connector.ready || isLoading}
-				className="textButton"
-				onClick={handleConnect}>
-				{name}
-				{connector.id === pendingConnector?.id && " (" + t("connecting") + ")"}
+	return isOpen ? <div className="modalOverlay">
+		<div className="txDoneModal">
+			<button
+				className="iconButton"
+				style={{
+					position: "absolute",
+					right: "16px",
+					top: "16px"
+				}}
+				onClick={onClose}>
+				<img src="images/close-x.png" />
 			</button>
-		})}
-	</Modal> : <></>
+
+			<img
+				className="illustration"
+				src="images/wallet-connection.png" />
+
+			<h2>{t("connectYourWallet")}</h2>
+
+			<div className="label">{t("connectYourWalletDescription")}</div>
+
+			{connectors.map((connector, index) => {
+				let name = connector.name;
+
+				// 判断是不是ioPay
+				if (name === "Trust Wallet" && window.navigator.userAgent.indexOf("IoPay") >= 0) {
+					name = "ioPay";
+				}
+
+				return <button
+					key={connector.name}
+					id={String(index)}
+					disabled={!connector.ready || isLoading}
+					className="secondaryButton bigButton"
+					onClick={handleConnect}>
+					{name}
+					{connector.id === pendingConnector?.id && " (" + t("connecting") + ")"}
+				</button>
+			})}
+		</div>
+	</div> : <></>
 };
