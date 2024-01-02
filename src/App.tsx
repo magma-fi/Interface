9b502493b7cbaf67608a0136e9fa838f6f3c551f
Yuushin <Shyy>
2024-01-02 13:22:45 +0800

    replace InjectedConnector with MetamaskConnector

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,9 @@ import { useEffect } from "react";
 import { configureChains, WagmiConfig, createConfig } from "wagmi";
 import { iotexTestnet, iotex } from "wagmi/chains";
 import { publicProvider } from "wagmi/providers/public";
-import { InjectedConnector } from "wagmi/connectors/injected";
+// import { InjectedConnector } from "wagmi/connectors/injected";
 import { WalletConnectConnector } from "wagmi/connectors/walletConnect";
+import { MetaMaskConnector } from "wagmi/connectors/metaMask";
 // import { jsonRpcProvider } from "@wagmi/core/providers/jsonRpc"
 // import { ConnectKitProvider, getDefaultClient } from "connectkit";
 // import { Flex, Heading, Paragraph, Link } from "theme-ui";
@@ -113,7 +114,10 @@ const { chains, publicClient, webSocketPublicClient } = configureChains(
 const wagmiCfg = createConfig({
   autoConnect: true,
   connectors: [
-    new InjectedConnector({ chains }),
+    new MetaMaskConnector({
+      chains,
+      options: { shimDisconnect: true }
+    }),
     new WalletConnectConnector({
       chains,
       options: {
