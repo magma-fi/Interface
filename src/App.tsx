601840df31a8f55f68da748274ac29fb2524574c
Yuushin <Shyy>
2024-01-03 15:17:26 +0800

    remove some useless code

--- src/App.tsx
+++ src/App.tsx
@@ -4,87 +4,18 @@ import { iotexTestnet, iotex } from "wagmi/chains";
 import { publicProvider } from "wagmi/providers/public";
 import { InjectedConnector } from "wagmi/connectors/injected";
 import { WalletConnectConnector } from "wagmi/connectors/walletConnect";
-// import { MetaMaskConnector } from "wagmi/connectors/metaMask";
-// import { jsonRpcProvider } from "@wagmi/core/providers/jsonRpc"
-// import { ConnectKitProvider, getDefaultClient } from "connectkit";
-// import { Flex, Heading, Paragraph, Link } from "theme-ui";
-
-// import { BatchedWebSocketAugmentedWeb3Provider } from "@liquity/providers";
 import { LiquityProvider } from "./hooks/LiquityContext";
-// import { WalletConnector } from "./components/WalletConnector";
-// import { TransactionProvider } from "./components/Transaction";
-// import { Icon } from "./components/Icon";
 import { getConfig } from "./config";
-// import theme from "./theme";
-
-// import { DisposableWalletProvider } from "./testUtils/DisposableWalletProvider";
 import { LiquityFrontend } from "./LiquityFrontend";
 import { AppLoader } from "./components/AppLoader";
 import { useAsyncValue } from "./hooks/AsyncValue";
 import { appController } from "./libs/appController";
 import { TransactionProvider } from "./components/Transaction";
-// import appConfig from "./appConfig.json";
-
-// const isDemoMode = import.meta.env.VITE_APP_DEMO_MODE === "true";
-
-// if (isDemoMode) {
-//   const ethereum = new DisposableWalletProvider(
-//     import.meta.env.VITE_APP_RPC_URL || `http://${window.location.hostname || "localhost"}:8545`,
-//     "0x4d5db4107d237df6a3d58ee5f70ae63d73d7658d4026f2eefd2f204c81682cb7"
-//   );
-
-//   Object.assign(window, { ethereum });
-// }
 
 // Start pre-fetching the config
 getConfig().then(config => {
-  // console.log("Frontend config:");
-  // console.log(config);
   Object.assign(window, { config });
 });
-
-// const UnsupportedMainnetFallback: React.FC = () => (
-//   <Flex
-//     sx={{
-//       flexDirection: "column",
-//       alignItems: "center",
-//       justifyContent: "center",
-//       height: "100vh",
-//       textAlign: "center"
-//     }}
-//   >
-//     <Heading sx={{ mb: 3 }}>
-//       <Icon name="exclamation-triangle" /> This app is for testing purposes only.
-//     </Heading>
-
-//     <Paragraph sx={{ mb: 3 }}>Please change your network to Görli.</Paragraph>
-
-//     <Paragraph>
-//       If you'd like to use the Liquity Protocol on mainnet, please pick a frontend{" "}
-//       <Link href="https://www.liquity.org/frontend">
-//         here <Icon name="external-link-alt" size="xs" />
-//       </Link>
-//       .
-//     </Paragraph>
-//   </Flex>
-// );
-
-// const UnsupportedNetworkFallback: React.FC = () => (
-//   <Flex
-//     sx={{
-//       flexDirection: "column",
-//       alignItems: "center",
-//       justifyContent: "center",
-//       height: "100vh",
-//       textAlign: "center"
-//     }}
-//   >
-//     <Heading sx={{ mb: 3 }}>
-//       <Icon name="exclamation-triangle" /> Liquity is not supported on this network.
-//     </Heading>
-//     Please switch to IoTeX Testnet.
-//   </Flex>
-// );
 
 const { chains, publicClient, webSocketPublicClient } = configureChains(
   [iotex, iotexTestnet],
@@ -94,30 +25,12 @@ const { chains, publicClient, webSocketPublicClient } = configureChains(
     //   rpc: (chain) => ({ http: appConfig.rpc[String(chain.id)].http })
     // })
   ],
-  // {
-  //   batch: {
-  //     multicall: {
-  //       batchSize: 512
-  //     }
-  //   },
-  //   pollingInterval: 10000,
-  //   rank: {
-  //     interval: 5000
-  //   },
-  //   retryCount: 5,
-  //   retryDelay: 3000,
-  //   stallTimeout: 5000
-  // }
   { batch: { multicall: true } }
 );
 
 const wagmiCfg = createConfig({
   connectors: [
     new InjectedConnector({ chains }),
-    // new MetaMaskConnector({
-    //   chains,
-    //   options: { shimDisconnect: true }
-    // }),
     new WalletConnectConnector({
       chains,
       options: {
@@ -135,63 +48,19 @@ const App = () => {
   const config = useAsyncValue(getConfig);
   const loader = <AppLoader />;
 
-  // return (
-  //   <ThemeUIProvider theme={theme}>
-  //     {config.loaded && (
-  //       <WagmiConfig
-  //         client={createClient(
-  //           getDefaultClient({
-  //             appName: "Liquity",
-  //             chains:
-  //               isDemoMode || import.meta.env.MODE === "test"
-  //                 ? [localhost]
-  //                 : config.value.testnetOnly
-  //                   ? [goerli]
-  //                   : [mainnet, goerli, iotexTestnet],
-  //             walletConnectProjectId: config.value.walletConnectProjectId,
-  //             infuraId: config.value.infuraApiKey,
-  //             alchemyId: config.value.alchemyApiKey
-  //           })
-  //         )}
-  //       >
-  //         <ConnectKitProvider options={{ hideBalance: true }}>
-  //           <WalletConnector loader={loader}>
-  //             <LiquityProvider
-  //               loader={loader}
-  //               unsupportedNetworkFallback={<UnsupportedNetworkFallback />}
-  //               unsupportedMainnetFallback={<UnsupportedMainnetFallback />}
-  //             >
-  //               <TransactionProvider>
-  //                 <LiquityFrontend loader={loader} />
-  //               </TransactionProvider>
-  //             </LiquityProvider>
-  //           </WalletConnector>
-  //         </ConnectKitProvider>
-  //       </WagmiConfig>
-  //     )}
-  //   </ThemeUIProvider>
-  // );
-
   useEffect(() => {
     appController.init();
   }, []);
 
   return config.loaded ? <WagmiConfig config={wagmiCfg}>
-    {/* <ConnectKitProvider options={{ hideBalance: true }}> */}
-    {/* <WalletConnector loader={loader}> */}
     <LiquityProvider
-      loader={loader}
-    // unsupportedNetworkFallback={<UnsupportedNetworkFallback />}
-    // unsupportedMainnetFallback={<UnsupportedMainnetFallback />}
-    >
+      loader={loader}>
       <TransactionProvider>
         <LiquityFrontend
           chains={chains}
           loader={loader} />
       </TransactionProvider>
     </LiquityProvider>
-    {/* </WalletConnector> */}
-    {/* </ConnectKitProvider> */}
   </WagmiConfig> : <></>
 };
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-// import Modal from "react-modal";
 
 import "./index.scss";
 import App from "./App";
-
-// Modal.setAppElement("#root");
 
 ReactDOM.render(
   <React.StrictMode>
