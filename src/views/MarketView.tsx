ae91d2b34784ffeed61bef9a6773748a5cfdf73c
Shyy Yuushin <siyushin@hotmail.com>
2024-11-08 07:26:35 +0800

    call borrowerOperation.claimRedeem to close vaults

--- src/views/MarketView.tsx
+++ src/views/MarketView.tsx
@@ -19,13 +19,14 @@ import { WithdrawModal } from "./WithdrawModal";
 import { TxDone } from "../components/TxDone";
 import { TxLabel } from "../components/TxLabel";
 import { graphqlAsker } from "../libs/graphqlAsker";
-import { useAccount } from "wagmi";
+import { Address, useAccount } from "wagmi";
 import { CloseModal } from "./CloseModal";
 import { TransactiionListItem } from "./TransactiionListItem";
 import appConfig from "../appConfig.json";
 import { useLiquity } from "../hooks/LiquityContext";
 import React from "react";
 import { appController } from "../libs/appController";
+import collSurplusPoolABI from "lib-ethers/abi/collSurplusPool.json";
 
 export const MarketView = ({
 	market,
@@ -76,7 +77,7 @@ export const MarketView = ({
 		fees,
 		lusdBalance
 	} = useLiquitySelector(selector);
-	const { walletClient, chainId, liquity, account } = useLiquity()
+	const { publicClient, walletClient, chainId, liquity, account } = useLiquity();
 	const [txHash, setTxHash] = useState("");
 	const [showDepositModal, setShowDepositModal] = useState(false);
 	const [depositAndBorrow, setDepositAndBorrow] = useState(true);
@@ -154,6 +155,24 @@ export const MarketView = ({
 	const [txs, setTxs] = useState<TroveChangeTx[]>([]);
 	const [changes, setChanges] = useState<TroveChangeData[]>([]);
 	const [chartBoxWidth, setChartBoxWidth] = useState(700)
+	const [collateralFromCollSurplusPool, setCollateralFromCollSurplusPool] = useState(Decimal.ZERO);
+
+	useEffect(() => {
+		const func = async () => {
+			if (trove.status === "closedByLiquidation" || trove.status === "closedByRedemption") {
+				const res: bigint = await publicClient?.readContract({
+					address: liquity.connection.addresses.collSurplusPool as Address,
+					abi: collSurplusPoolABI,
+					functionName: "getCollateral",
+					args: [account]
+				}) as bigint;
+
+				setCollateralFromCollSurplusPool(Decimal.from(res.toString()));
+			}
+		};
+
+		func();
+	}, [account, liquity.connection.addresses.collSurplusPool, publicClient, trove]);
 
 	useEffect(() => {
 		if (!account) return;
@@ -360,281 +379,290 @@ export const MarketView = ({
 			id="marketView"
 			className="marketView marketViewLayout">
 			<div>
-				{trove.status !== "open" && <div className="card">
-					<img className="illustration" src="images/1wen=1usd.png" />
+				{(
+					trove.status === "nonExistent" ||
+					trove.status === "closedByOwner" ||
+					trove.status === "closedByLiquidation" ||
+					(trove.status === "closedByRedemption" && collateralFromCollSurplusPool.eq(0))
+				) && <div className="card">
+						<img className="illustration" src="images/1wen=1usd.png" />
 
-					<div>
-						<h3>{t("letsGetStarted")}</h3>
+						<div>
+							<h3>{t("letsGetStarted")}</h3>
 
-						<p className="description">{t("letsGetStartedDEscription", {
-							interest: "0%",
-							percent: feePct.toString(2)
-						})}</p>
-					</div>
+							<p className="description">{t("letsGetStartedDEscription", {
+								interest: "0%",
+								percent: feePct.toString(2)
+							})}</p>
+						</div>
 
-					<button
-						id="0"
-						className="primaryButton bigButton"
-						style={{ width: "100%" }}
-						onClick={handleDeposit}
-						disabled={accountBalance.eq(0)}>
-						<img src="images/deposit.png" />
+						<button
+							id="0"
+							className="primaryButton bigButton"
+							style={{ width: "100%" }}
+							onClick={handleDeposit}
+							disabled={accountBalance.eq(0) || trove.status === "closedByLiquidation" || trove.status === "closedByRedemption"}>
+							<img src="images/deposit.png" />
 
-						{t("deposit") + " " + market?.symbol}
-					</button>
+							{t("deposit") + " " + market?.symbol}
+						</button>
 
-					<div
-						className="description"
-						style={{
-							width: "100%",
-							textAlign: "center"
-						}}>{t("walletBalance")}&nbsp;{accountBalance.toString(2)}&nbsp;{market?.symbol}</div>
-				</div>}
+						<div
+							className="description"
+							style={{
+								width: "100%",
+								textAlign: "center"
+							}}>{t("walletBalance")}&nbsp;{accountBalance.toString(2)}&nbsp;{market?.symbol}</div>
+					</div>}
 
-				{trove.status === "open" && <div
+				{(
+					trove.status === "open" ||
+					((trove.status === "closedByLiquidation" || trove.status === "closedByRedemption") && collateralFromCollSurplusPool.gt(0))
+				) && <div
 					className="card"
 					style={{ paddingTop: "0.5rem" }}>
-					<div
-						className="flex-row-space-between"
-						style={{ alignItems: "center" }}>
-						<h3>{t("yourVault")}</h3>
-
-						<DropdownMenu
-							options={TroveOptions}
-							onChange={handleTroveAction}>
-							<IconButton icon="images/dots.png" />
-						</DropdownMenu>
-					</div>
-
-					<div className="charts">
 						<div
-							className="subCard"
-							style={{ minHeight: "190px" }}>
-							<div className="flex-row-space-between">
-								<div className="label">{t("liquidation")}</div>
+							className="flex-row-space-between"
+							style={{ alignItems: "center" }}>
+							<h3>{t("yourVault")}</h3>
 
-								<img
-									src="images/liquidations.png"
-									width="20px" />
-							</div>
-
-							<div className="flex-column-align-left">
-								<div>{price.gt(liquidationPrice) ? price.sub(liquidationPrice).div(price).mul(100).toString(2) : 0}%</div>
-								<div className="label labelSmall">{t("belowCurrentPrice")}</div>
-							</div>
-
-							<div className="flex-column-align-left">
-								<div>{liquidationPrice.toString(5)}&nbsp;{globalContants.USD}</div>
-
-								<div className="label labelSmall">{t("liquidationPrice")}</div>
-							</div>
-
-							<div className="label labelSmall">{t("currentPrice")}:&nbsp;{price.toString(5)}&nbsp;{globalContants.USD}</div>
+							<DropdownMenu
+								options={TroveOptions}
+								onChange={handleTroveAction}>
+								<IconButton icon="images/dots.png" />
+							</DropdownMenu>
 						</div>
 
-						<div
-							className="subCard"
-							style={{ minHeight: "190px" }}>
-							<div className="label">{t("utilizationRate")}</div>
+						<div className="charts">
+							<div
+								className="subCard"
+								style={{ minHeight: "190px" }}>
+								<div className="flex-row-space-between">
+									<div className="label">{t("liquidation")}</div>
 
-							<div className="chartContainer">
-								<PieChart width={108} height={108}>
-									<Pie
-										data={chartData}
-										cx="50%"
-										cy="50%"
-										innerRadius={40}
-										outerRadius={54}
-										stroke="rgba(255, 255, 255, 0.1)"
-										paddingAngle={0}
-										dataKey="value"
-										startAngle={0}
-										endAngle={360}>
-										{chartData.map((entry, index) => (
-											<Cell
-												key={index}
-												fill={COLORS[index % COLORS.length]} />
-										))}
-									</Pie>
-
-									{needle(Number(mcrPercent.mul(360)), 50, 50, '#F25454')}
-								</PieChart>
-
-								<div className="label">{troveUtilizationRateNumber.toFixed(2)}%</div>
-							</div>
-
-							<div className="flex-column">
-								<div className="flex-row-align-left label labelSmall">
-									<div className="label labelSmall">{t("liquidationAt")}</div>
-
-									<div style={{ color: "#F25454" }}>{mcrPercent.mul(100).toString(2)}%</div>
+									<img
+										src="images/liquidations.png"
+										width="20px" />
 								</div>
-							</div>
-						</div>
-					</div>
-
-					<div
-						className="flex-row-space-between"
-						style={{ alignItems: "flex-end" }}>
-						<div className="flex-column-align-left">
-							<div className="label">{t("deposited")}</div>
-
-							<div className="flex-row-align-left">
-								<img
-									src="images/iotx.png"
-									width="40px" />
 
 								<div className="flex-column-align-left">
-									<div>{troveCollateralValue.toString(2)}&nbsp;{globalContants.USD}</div>
-
-									<div className="label labelSmall">{trove.collateral.toString(2)}&nbsp;{IOTX.symbol}</div>
+									<div>{price.gt(liquidationPrice) ? price.sub(liquidationPrice).div(price).mul(100).toString(2) : 0}%</div>
+									<div className="label labelSmall">{t("belowCurrentPrice")}</div>
 								</div>
+
+								<div className="flex-column-align-left">
+									<div>{liquidationPrice.toString(5)}&nbsp;{globalContants.USD}</div>
+
+									<div className="label labelSmall">{t("liquidationPrice")}</div>
+								</div>
+
+								<div className="label labelSmall">{t("currentPrice")}:&nbsp;{price.toString(5)}&nbsp;{globalContants.USD}</div>
 							</div>
-						</div>
-
-						<div
-							// style={{ flex: "1 1" }}
-							className="flex-column-align-right"
-							style={{ gap: "5px" }}>
-							<button
-								id="1"
-								className="secondaryButton"
-								onClick={handleDeposit}>
-								<img src="images/deposit-light.png" />
-
-								{t("deposit") + " " + IOTX.symbol}
-							</button>
 
 							<div
-								className="label labelSmall"
-								style={{
-									textAlign: "center",
-									width: "100%"
-								}}>{t("balance")}&nbsp;{accountBalance.toString(2)}&nbsp;{IOTX.symbol}</div>
-						</div>
-					</div>
+								className="subCard"
+								style={{ minHeight: "190px" }}>
+								<div className="label">{t("utilizationRate")}</div>
 
-					<div
-						className="flex-row-space-between"
-						style={{ alignItems: "flex-end" }}>
-						<div className="flex-column-align-left">
-							<div className="label">{t("available2Borrow")}</div>
+								<div className="chartContainer">
+									<PieChart width={108} height={108}>
+										<Pie
+											data={chartData}
+											cx="50%"
+											cy="50%"
+											innerRadius={40}
+											outerRadius={54}
+											stroke="rgba(255, 255, 255, 0.1)"
+											paddingAngle={0}
+											dataKey="value"
+											startAngle={0}
+											endAngle={360}>
+											{chartData.map((entry, index) => (
+												<Cell
+													key={index}
+													fill={COLORS[index % COLORS.length]} />
+											))}
+										</Pie>
 
-							<div className="flex-row-align-left">
-								<img
-									src={WEN.logo}
-									width="40px" />
+										{needle(Number(mcrPercent.mul(360)), 50, 50, '#F25454')}
+									</PieChart>
 
-								<div className="flex-column-align-left">
-									<div>{availableBorrow.toString(2)}&nbsp;{globalContants.USD}</div>
+									<div className="label">{troveUtilizationRateNumber.toFixed(2)}%</div>
+								</div>
 
-									<div className="label labelSmall">{availableBorrow.toString(2)}&nbsp;{WEN.symbol}</div>
+								<div className="flex-column">
+									<div className="flex-row-align-left label labelSmall">
+										<div className="label labelSmall">{t("liquidationAt")}</div>
+
+										<div style={{ color: "#F25454" }}>{mcrPercent.mul(100).toString(2)}%</div>
+									</div>
 								</div>
 							</div>
 						</div>
 
 						<div
-							className="flex-column-align-right"
-							style={{ gap: "5px" }}>
-							<button
-								className="primaryButton"
-								onClick={handleBorrow}
-								disabled={availableBorrow.lt(0.01)}>
-								<img src="images/borrow-dark.png" />
+							className="flex-row-space-between"
+							style={{ alignItems: "flex-end" }}>
+							<div className="flex-column-align-left">
+								<div className="label">{t("deposited")}</div>
 
-								{t("borrow") + " " + WEN.symbol}
-							</button>
+								<div className="flex-row-align-left">
+									<img
+										src="images/iotx.png"
+										width="40px" />
+
+									<div className="flex-column-align-left">
+										<div>{troveCollateralValue.toString(2)}&nbsp;{globalContants.USD}</div>
+
+										<div className="label labelSmall">{trove.collateral.toString(2)}&nbsp;{IOTX.symbol}</div>
+									</div>
+								</div>
+							</div>
 
 							<div
-								className="label labelSmall"
-								style={{
-									textAlign: "center",
-									width: "100%"
-								}}>{t("currentFee")}&nbsp;{borrowingFeePct.toString(2)}</div>
-						</div>
-					</div>
+								// style={{ flex: "1 1" }}
+								className="flex-column-align-right"
+								style={{ gap: "5px" }}>
+								<button
+									id="1"
+									className="secondaryButton"
+									onClick={handleDeposit}
+									disabled={trove.status === "closedByLiquidation" || trove.status === "closedByRedemption"}>
+									<img src="images/deposit-light.png" />
 
-					<div
-						className="flex-row-space-between"
-						style={{ alignItems: "flex-end" }}>
-						<div className="flex-column-align-left">
-							<div className="label">{t("debt")}</div>
+									{t("deposit") + " " + IOTX.symbol}
+								</button>
 
-							<div className="flex-row-align-left">
-								<img
-									src={WEN.logo}
-									width="40px" />
-
-								<div className="flex-column-align-left">
-									<div>{trove.debt.toString(2)}&nbsp;{globalContants.USD}</div>
-
-									<div className="label labelSmall">{trove.debt.toString(2)}&nbsp;{WEN.symbol}</div>
-								</div>
+								<div
+									className="label labelSmall"
+									style={{
+										textAlign: "center",
+										width: "100%"
+									}}>{t("balance")}&nbsp;{accountBalance.toString(2)}&nbsp;{IOTX.symbol}</div>
 							</div>
 						</div>
 
 						<div
-							className="flex-column-align-right"
-							style={{ gap: "5px" }}>
-							<button
-								className="secondaryButton"
-								onClick={handleRepay}
-								disabled={maxAvailableRepay.lt(0.01) || lusdBalance.eq(0)}>
-								<img src="images/repay.png" />
+							className="flex-row-space-between"
+							style={{ alignItems: "flex-end" }}>
+							<div className="flex-column-align-left">
+								<div className="label">{t("available2Borrow")}</div>
 
-								{t("repay") + " " + WEN.symbol}
-							</button>
+								<div className="flex-row-align-left">
+									<img
+										src={WEN.logo}
+										width="40px" />
 
-							{(maxAvailableRepay.lt(0.01) || lusdBalance.eq(0)) && <div
-								className="label labelSmall"
-								style={{
-									textAlign: "center",
-									width: "100%"
-								}}
-								title={maxAvailableRepay.lt(0.01) ? t("minimumLoanReachedSo") : ""}>
-								{maxAvailableRepay.lt(0.01)
-									? t("minimumLoanReached")
-									: (lusdBalance.eq(0) && " " + WEN.symbol + " " + t("balance") + ": 0")}
-							</div>}
-						</div>
-					</div>
+									<div className="flex-column-align-left">
+										<div>{availableBorrow.toString(2)}&nbsp;{globalContants.USD}</div>
 
-					<div
-						className="flex-row-space-between"
-						style={{
-							alignItems: "flex-end",
-							gap: "none"
-						}}>
-						<div className="flex-column-align-left">
-							<div className="label">{t("available2Withdraw")}</div>
-
-							<div className="flex-row-align-left">
-								<img
-									src="images/iotx.png"
-									width="40px" />
-
-								<div className="flex-column-align-left">
-									<div>{availableWithdrawalFiat.gt(0) ? availableWithdrawalFiat.toString(2) : "0"}&nbsp;{globalContants.USD}</div>
-
-									<div className="label labelSmall">{availableWithdrawal.gt(0) ? availableWithdrawal.toString(2) : "0"}&nbsp;{IOTX.symbol}</div>
+										<div className="label labelSmall">{availableBorrow.toString(2)}&nbsp;{WEN.symbol}</div>
+									</div>
 								</div>
+							</div>
+
+							<div
+								className="flex-column-align-right"
+								style={{ gap: "5px" }}>
+								<button
+									className="primaryButton"
+									onClick={handleBorrow}
+									disabled={availableBorrow.lt(0.01)}>
+									<img src="images/borrow-dark.png" />
+
+									{t("borrow") + " " + WEN.symbol}
+								</button>
+
+								<div
+									className="label labelSmall"
+									style={{
+										textAlign: "center",
+										width: "100%"
+									}}>{t("currentFee")}&nbsp;{borrowingFeePct.toString(2)}</div>
 							</div>
 						</div>
 
 						<div
-							className="flex-column-align-right"
-							style={{ gap: "5px" }}>
-							<button
-								className="secondaryButton"
-								onClick={handleWithdraw}
-								disabled={availableWithdrawal.lt(0.01)}>
-								<img src="images/withdraw.png" />
+							className="flex-row-space-between"
+							style={{ alignItems: "flex-end" }}>
+							<div className="flex-column-align-left">
+								<div className="label">{t("debt")}</div>
 
-								{t("withdraw") + " " + IOTX.symbol}
-							</button>
+								<div className="flex-row-align-left">
+									<img
+										src={WEN.logo}
+										width="40px" />
+
+									<div className="flex-column-align-left">
+										<div>{trove.debt.toString(2)}&nbsp;{globalContants.USD}</div>
+
+										<div className="label labelSmall">{trove.debt.toString(2)}&nbsp;{WEN.symbol}</div>
+									</div>
+								</div>
+							</div>
+
+							<div
+								className="flex-column-align-right"
+								style={{ gap: "5px" }}>
+								<button
+									className="secondaryButton"
+									onClick={handleRepay}
+									disabled={maxAvailableRepay.lt(0.01) || lusdBalance.eq(0)}>
+									<img src="images/repay.png" />
+
+									{t("repay") + " " + WEN.symbol}
+								</button>
+
+								{(maxAvailableRepay.lt(0.01) || lusdBalance.eq(0)) && <div
+									className="label labelSmall"
+									style={{
+										textAlign: "center",
+										width: "100%"
+									}}
+									title={maxAvailableRepay.lt(0.01) ? t("minimumLoanReachedSo") : ""}>
+									{maxAvailableRepay.lt(0.01)
+										? t("minimumLoanReached")
+										: (lusdBalance.eq(0) && " " + WEN.symbol + " " + t("balance") + ": 0")}
+								</div>}
+							</div>
 						</div>
-					</div>
-				</div>}
+
+						<div
+							className="flex-row-space-between"
+							style={{
+								alignItems: "flex-end",
+								gap: "none"
+							}}>
+							<div className="flex-column-align-left">
+								<div className="label">{t("available2Withdraw")}</div>
+
+								<div className="flex-row-align-left">
+									<img
+										src="images/iotx.png"
+										width="40px" />
+
+									<div className="flex-column-align-left">
+										<div>{availableWithdrawalFiat.gt(0) ? availableWithdrawalFiat.toString(2) : "0"}&nbsp;{globalContants.USD}</div>
+
+										<div className="label labelSmall">{availableWithdrawal.gt(0) ? availableWithdrawal.toString(2) : "0"}&nbsp;{IOTX.symbol}</div>
+									</div>
+								</div>
+							</div>
+
+							<div
+								className="flex-column-align-right"
+								style={{ gap: "5px" }}>
+								<button
+									className="secondaryButton"
+									onClick={handleWithdraw}
+									disabled={trove.status === "open" && availableWithdrawal.lt(0.01)}>
+									<img src="images/withdraw.png" />
+
+									{t("withdraw") + " " + IOTX.symbol}
+								</button>
+							</div>
+						</div>
+					</div>}
 			</div>
 
 			<div
@@ -916,7 +944,8 @@ export const MarketView = ({
 			recoveryMode={recoveryMode}
 			// liquidationPoint={liquidationPoint}
 			liquidationPoint={appLiquidationPoint}
-			availableBorrow={availableBorrow} />}
+			availableBorrow={availableBorrow}
+			collateralFromCollSurplusPool={collateralFromCollSurplusPool} />}
 
 		{showWithdrawDoneModal && <TxDone
 			title={t("withdrawnSuccessfully")}
@@ -938,6 +967,7 @@ export const MarketView = ({
 			validationContext={validationContext}
 			chainId={chainId}
 			balance={accountBalance}
-			price={price} />}
+			price={price}
+			collateralFromCollSurplusPool={collateralFromCollSurplusPool} />}
 	</>
 };
