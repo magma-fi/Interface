7f83b14d028bc6c72e0c1f89b41ebf40df31cf66
Shyy Yuushin <siyushin@hotmail.com>
2024-11-08 13:14:12 +0800

    pick up the funcions of redeeming collateral from the branch features_redeem into main-v2

--- src/views/WithdrawModal.tsx
+++ src/views/WithdrawModal.tsx
@@ -180,12 +180,13 @@ export const WithdrawModal = ({
 			functionName: "claimCollateral",
 			args: []
 		});
+		window.location.reload();
 	};
 
 	return isOpen ? <Modal
 		title={t("withdraw") + " " + market.symbol}
 		onClose={handleCloseModal}>
-		<div className="withdrawModal">
+		{trove.status !== "closedByLiquidation" && trove.status !== "closedByRedemption" && <div className="withdrawModal">
 			<div className="flex-column">
 				<div className="flex-column-align-left">
 					<div
@@ -283,7 +284,9 @@ export const WithdrawModal = ({
 					</div>
 				</div>
 			</div>
-		</div>
+
+			<p className="tips">{t("tips4Down")}</p>
+		</div>}
 
 		{stableTroveChange && (
 			(!transactionState.id && transactionState.type === "idle")
@@ -312,7 +315,7 @@ export const WithdrawModal = ({
 			onClick={handleClaimCollateral}>
 			<img src="images/repay-dark.png" />
 
-			{t("withdraw")}
+			{t("claimCollateral") + " " + collateralFromCollSurplusPool.toString(2) + " " + market.symbol}
 		</button> : <button
 			className="primaryButton bigButton"
 			style={{ width: "100%" }}
