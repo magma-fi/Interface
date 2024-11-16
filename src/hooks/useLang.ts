0fc755043017b9a2c5b859ce0e53149f784c9028
Shyy Yuushin <siyushin@hotmail.com>
2024-11-16 12:25:06 +0800

    update useLang.ts

--- src/hooks/useLang.ts
+++ src/hooks/useLang.ts
@@ -1,33 +1,37 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { useEffect, useState } from "react";
 import { Langs } from "../libs/globalContants";
 import { appController } from "../libs/appController";
 
-let currentLang: Langs = Langs.English;
-// let localizedStrings: Record<string, string> | null = null;
+const localizedStringsBundle: Record<string, any> = {};
 
 export function useLang() {
 	const appLang = appController.lang;
 	const [localizedStrings, setLocalizedStrings] = useState(null);
 
-	const getStrings = async () => {
+	const getStrings = async (lang: Langs) => {
 		let res = null;
 		try {
-			res = await (await fetch("/locale/" + currentLang + ".json")).json();
+			res = await (await fetch("/locale/" + lang + ".json")).json();
 		} catch (error) {
 			console.error(error);
 		}
 
+		localizedStringsBundle[lang] = res;
 		setLocalizedStrings(res);
 	};
 
 	useEffect(() => {
-		currentLang = appLang;
-		getStrings();
+		if (localizedStringsBundle[appLang]) {
+			setLocalizedStrings(localizedStringsBundle[appLang]);
+		} else {
+			getStrings(appLang);
+		}
 	}, [appLang]);
 
 	const t = (key: string, valuesObject: (Record<string, string> | null) = null) => {
 		if (!localizedStrings) {
-			return "";
+			return key;
 		} else {
 			const str = (localizedStrings && key) ? localizedStrings[key] : "";
 			return valuesObject ? str.replace(/\{(\w+)\}/g, (match, key) => {
